b611c0a484a2a81dca7e86475b822be0a1b3f7a8
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-10-26 20:58:21 -0700

    homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/dashboard', withAuth, async (req, res, next) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+  
+    res.render('dashboard', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
